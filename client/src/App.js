587be6ca93e8c2b9a2b0e0c3c440dadee258f36a
Brennan Davis <brennantd@gmail.com>
2022-10-26 16:21:21 -0600

    added tailwind and flowbite

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,18 +12,21 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route exact path="/">
-            <h1>Page Count: {count}</h1>
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <>
+      <NavBar />
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/testing">
+              <h1 className="text-3xl font-bold underline">Test Route</h1>
+            </Route>
+            <Route exact path="/">
+              <h1 className="text-3xl font-bold underline">Page Count: {count}</h1>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
