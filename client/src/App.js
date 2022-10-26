9db60f694f99bc787a51fd5216089660b0ed51d9
Brennan Davis <brennantd@gmail.com>
2022-10-26 11:39:54 -0600

    template setup complete

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/testing">
+            <h1>Test Route</h1>
+          </Route>
+          <Route exact path="/">
+            <h1>Page Count: {count}</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
