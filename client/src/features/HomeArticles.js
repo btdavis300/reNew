84dafe5bf04d7b2d14b7bc2cd52fe30aefd1abb9
Brennan Davis <brennantd@gmail.com>
2022-11-01 14:44:40 -0600

    added image placeholder to Main page articles w/o images in json

--- client/src/features/HomeArticles.js
+++ client/src/features/HomeArticles.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import imagePlaceholder from '../assets/news-article-placeholder.png'
 
 function HomeArticles({ article }) {
 
     return (
         <div>
             <h1>{article.title}</h1>
-            {article.multimedia ? <img src={article.multimedia[0].url} width="600" height="400" alt="front page article" /> : null}
+            <img src={article.multimedia ? article.multimedia[0].url : imagePlaceholder} width="600" height="400" alt="front page article" />
             <p>{article.abstract}</p>
         </div>
     )
