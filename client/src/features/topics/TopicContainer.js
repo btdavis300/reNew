a411c3e6fc24a2b1b7cccc266753e6c61c76ae6d
Brennan Davis <brennantd@gmail.com>
2022-11-02 09:50:01 -0600

    topiceTitle slice state displayed in Topic Container. still need to debug async rendering upon initial load

--- client/src/features/topics/TopicContainer.js
+++ client/src/features/topics/TopicContainer.js
@@ -4,6 +4,7 @@ import MainArticle from './MainArticle';
 import TopicArticles from './TopicArticles';
 
 function TopicContainer() {
+    const title = useSelector(state => state.topicTitle.title)
     const articles = useSelector(state => state.newsTopic.entities)
     const mainArt = articles[0]
     const topicArts = articles.slice(1)
@@ -13,8 +14,11 @@ function TopicContainer() {
     const topicArticles = topicArts.map(a => <TopicArticles key={a.title} article={a} />)
 
   return (
-    <div className='flex flex-col items-center p-10'>
+    <div className='flex flex-col items-center px-10'>
       <div className='w-11/12'>
+        <div className='font-bold italic text-3xl  py-3 border-b'>
+          {title}
+        </div>
       <div>
       {/* <a href={mainArt.url} target="_blank"> */}
         <MainArticle article={mainArt}/>
