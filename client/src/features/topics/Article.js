73590586dbff9e2499c8c5570763726c6bf2fa7b
Brennan Davis <brennantd@gmail.com>
2022-11-02 16:36:47 -0600

    added comment display to article page. WIP

--- client/src/features/topics/Article.js
+++ client/src/features/topics/Article.js
@@ -21,13 +21,24 @@ function Article() {
                     </div>
                 </div>
                 <div className='pt-10'>
-                    <form>
-                        <div class="mb-6">
-                            <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Write Comment</label>
-                            <textarea type="text" id="large-input" rows="7" cols="100" class="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                        </div>
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Post</button>
-                    </form>
+                    <div>
+                        <form>
+                            <div class="mb-6">
+                                <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Write Comment</label>
+                                <textarea type="text" id="large-input" rows="3" cols="100" class="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            </div>
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Post</button>
+                        </form>
+                    </div>
+                    <div className='pt-7'>
+                        <ul>
+                            <li>Comment 1</li>
+                            <li>Comment 2</li>
+                            <li>Comment 3</li>
+                            <li>Comment 4</li>
+                            <li>Comment 5</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
