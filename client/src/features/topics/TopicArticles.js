384b835cf8c2049fc94c141e75eeed9ad9222cde
Brennan Davis <brennantd@gmail.com>
2022-11-02 08:51:24 -0600

    styling to TopicArticles

--- client/src/features/topics/TopicArticles.js
+++ client/src/features/topics/TopicArticles.js
@@ -3,14 +3,14 @@ import imagePlaceholder from '../../assets/news-article-placeholder.png'
 
 function TopicArticles({ article }) {
   return (
-    <div className='w-1/3 p-5'>
+    <div className='w-1/3 px-5 py-10 border-y'>
         <div>
         <img src={article.multimedia ? article.multimedia[1].url : imagePlaceholder}  alt="front page article" />
         </div>
-        <div>
-            <h1>{article.title}</h1>
-            <p>{article.abstract}</p>
-            <h5>{article.byline}</h5>
+        <div className='flex flex-col hover:text-slate-600'>
+            <h1 className='text-xl italic font-semi-bold hover:underline pt-5'>{article.title}</h1>
+            <p className='text-slate-600 pt-2'>{article.abstract}</p>
+            <h5 className='text-slate-600 text-xs pt-1'>{article.byline}</h5>
         </div>
     </div>
   )
