700485297317fccd7f048fabf64fcf2c93b4fd54
Brennan Davis <brennantd@gmail.com>
2022-11-03 14:29:00 -0600

    published date displayed on Article page

--- client/src/features/topics/Article.js
+++ client/src/features/topics/Article.js
@@ -5,29 +5,44 @@ import imagePlaceholder from '../../assets/news-article-placeholder.png'
 function Article() {
     const article = useSelector(state => state.article.entity)
 
+    const pubdate = article.published_date
+    const date = pubdate.indexOf('T')
+    const newDate = pubdate.slice(0, date)
+
+
+
     return (
-        <div className='flex justify-center'>
-            <div className='flex flex-row w-3/4'>
-                <div className='flex justify-center px-10'>
-                    <div className='flex flex-col items-center'>
-                        <div className='pt-10 text-center'>
-                            <h1 className='text-5xl italic font-semibold'>{article.title}</h1>
-                            <h5 className='pb-7 pt-3 text-lg italic'>{article.byline}</h5>
-                        </div>
-                        <div className='w-2/3 flex flex-col items-center'>
-                            <img src={article.multimedia ? article.multimedia[1].url : imagePlaceholder} alt="front page article" />
-                            <p className='pt-7 text-slate-600 text-xl'>{article.abstract}</p>
+        <div>
+            <div className='flex justify-center pt-20'>
+                <div className='flex flex-row items-center w-3/4 border-b pb-7'>
+                    <div className='px-10'>
+                        <div>
+                            <div>
+                                <h1 className='text-4xl italic font-semibold text-center'>{article.title}</h1>
+                                <ul className='text-slate-600 text-sm'>
+                                    <li className='pt-3'>{article.byline}</li>
+                                    <li className='pb-7'>{newDate}</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <p className='text-slate-600 text-xl'>{article.abstract}</p>
+                            </div>
                         </div>
                     </div>
+                    <div>
+                        <img src={article.multimedia ? article.multimedia[0].url : imagePlaceholder} alt="front page article" />
+                    </div>
                 </div>
-                <div className='pt-10'>
+            </div>
+            <div className='flex justify-center pt-7'>
+                <div className='w-1/2 flex flex-col'>
                     <div>
                         <form>
-                            <div class="mb-6">
-                                <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Write Comment</label>
-                                <textarea type="text" id="large-input" rows="3" cols="100" class="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            <div className="mb-6">
+                                <label htmlFor="large-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Write Comment</label>
+                                <textarea type="text" id="large-input" rows="3" cols="100" className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
-                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Post</button>
+                            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Post</button>
                         </form>
                     </div>
                     <div className='pt-7'>
